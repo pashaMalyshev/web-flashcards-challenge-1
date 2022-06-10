548f9476b298247d57e9d7345dab063aac238129
Pavel <gomer_74@mail.ru>
2022-06-10 13:15:10 +0300

    add migration and model

---
+++ db/models/card.js
@@ -0,0 +1,53 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Card extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Topic }) {
+      // define association here
+      Card.belongsTo(Topic, { foreignKey: 'topic_id' });
+    }
+  }
+  Card.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    question: {
+      allowNull: false,
+      type: DataTypes.TEXT,
+    },
+    answer: {
+      allowNull: false,
+      type: DataTypes.TEXT,
+    },
+    topic_id: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Topics',
+        key: 'id',
+      },
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  }, {
+    sequelize,
+    modelName: 'Card',
+  });
+  return Card;
+};
